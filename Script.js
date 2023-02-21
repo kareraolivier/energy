d1a28af0d1a94a0276a09f7aa88a62cd2e18f090
kareraolivier <kareraolivier@gmail.com>
2023-02-21 05:26:26 +0200

    energy

---
+++ Script.js
@@ -0,0 +1,335 @@
+const select = document.getElementById("hf");
+
+select.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 3) {
+    select.style.color = "green";
+  } else {
+    select.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(select.options[select.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(select.options[select.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectsf = document.getElementById("sf");
+
+selectsf.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selectsf.style.color = "green";
+  } else {
+    selectsf.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(select.options[selectsf.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(select.options[selectsf.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectwf = document.getElementById("wf");
+
+selectwf.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 2) {
+    selectwf.style.color = "green";
+  } else {
+    selectwf.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectwf.options[selectwf.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectwf.options[selectwf.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectnf = document.getElementById("nf");
+
+selectnf.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 2) {
+    selectnf.style.color = "green";
+  } else {
+    selectnf.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectnf.options[selectnf.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectnf.options[selectnf.selectedIndex].text);
+});
+
+//.......................................................
+
+const selecths = document.getElementById("hs");
+
+selecths.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selecths.style.color = "green";
+  } else {
+    selecths.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selecths.options[selecths.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selecths.options[selecths.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectss = document.getElementById("ss");
+
+selectss.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selectss.style.color = "green";
+  } else {
+    selectss.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectss.options[selectss.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectss.options[selectss.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectws = document.getElementById("ws");
+
+selectws.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selectws.style.color = "green";
+  } else {
+    selectws.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectws.options[selectws.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectws.options[selectws.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectns = document.getElementById("ns");
+
+selectns.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 2) {
+    selectns.style.color = "green";
+  } else {
+    selectns.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectns.options[selectns.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectns.options[selectns.selectedIndex].text);
+});
+
+//.......................................................
+
+const selecthp = document.getElementById("hp");
+
+selecthp.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 2) {
+    selecthp.style.color = "green";
+  } else {
+    selecthp.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selecthp.options[selecthp.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selecthp.options[selecthp.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectsp = document.getElementById("sp");
+
+selectsp.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selectsp.style.color = "green";
+  } else {
+    selectsp.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectsp.options[selectsp.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectsp.options[selectsp.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectwp = document.getElementById("wp");
+
+selectwp.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 3) {
+    selectwp.style.color = "green";
+  } else {
+    selectwp.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectwp.options[selectwp.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectwp.options[selectwp.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectnp = document.getElementById("np");
+
+selectnp.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 3) {
+    selectnp.style.color = "green";
+  } else {
+    selectnp.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectnp.options[selectnp.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectnp.options[selectnp.selectedIndex].text);
+});
+
+//.......................................................
+
+const selecthe = document.getElementById("he");
+
+selecthe.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selecthe.style.color = "green";
+  } else {
+    selecthe.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selecthe.options[selecthe.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selecthe.options[selecthe.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectse = document.getElementById("se");
+
+selectse.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selectse.style.color = "green";
+  } else {
+    selectse.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectse.options[selectse.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectse.options[selectse.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectwe = document.getElementById("we");
+
+selectwe.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 1) {
+    selectwe.style.color = "green";
+  } else {
+    selectwe.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectwe.options[selectwe.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectwe.options[selectwe.selectedIndex].text);
+});
+
+//.......................................................
+
+const selectne = document.getElementById("ne");
+
+selectne.addEventListener("change", function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  let num = event.target.value;
+  if (num == 2) {
+    selectne.style.color = "green";
+  } else {
+    selectne.style.color = "red";
+  }
+  // 👇️ get selected VALUE even outside event handler
+  console.log(selectne.options[selectne.selectedIndex].value);
+
+  // 👇️ get selected TEXT in or outside event handler
+  console.log(selectne.options[selectne.selectedIndex].text);
+});
+
+const image = document.getElementById("image");
+const noimage = document.getElementById("noimage");
+
+function check() {
+  if (document.getElementById("checkone").checked) {
+    image.innerHTML = `<div><h2>You answered questions correctly</h2>
+    <img src="./image/perfect-young-dylan bbb.gif" alt="no" width="250" class="image" >
+    </div>
+    `;
+  } else if (document.getElementById("checktwo").checked) {
+    image.innerHTML = `<div><h2>You answered questions incorrectly</h2>
+   <img src="./image/bart-simpson-the-simpsons.gif" alt="ok" width="250" class="image">
+ </div>   `;
+  } else if (document.getElementById("checkthree").checked) {
+    image.innerHTML = `<div><h2>You answered questions incorrectly</h2>
+   <img src="./image/bart-simpson-the-simpsons.gif" alt="ok" width="250" class="image">
+ </div>   `;
+  } else if (document.getElementById("checkfour").checked) {
+    image.innerHTML = `<div><h2>You answered questions incorrectly</h2>
+   <img src="./image/bart-simpson-the-simpsons.gif" alt="ok" width="250" class="image">
+ </div>   `;
+  } else {
+    noimage.innerHTML = `<h2>You have not answered any questions</h2>
+  `;
+  }
+}
+
+function uncheck() {
+  document.getElementById("checkone").checked = false;
+  document.getElementById("checktwo").checked = false;
+  document.getElementById("checkthree").checked = false;
+  document.getElementById("checkfour").checked = false;
+}
